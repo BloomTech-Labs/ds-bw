2f595ea1fd45af8a16328d65a1e5badf180b6b39
Ryan Herr <rrherr@gmail.com>
2020-08-18 15:02:48 -0500

    Update predict function docstring

--- app/api/predict.py
+++ app/api/predict.py
@@ -29,7 +29,19 @@ class Item(BaseModel):
 
 @router.post('/predict')
 async def predict(item: Item):
-    """Make random baseline predictions for classification problem."""
+    """
+    Make random baseline predictions for classification problem 🔮
+    ### Request Body
+    - `x1`: positive float
+    - `x2`: integer
+    - `x3`: string
+    ### Response
+    - `prediction`: boolean, at random
+    - `predict_proba`: float between 0.5 and 1.0, 
+    representing the predicted class's probability
+    Replace the placeholder docstring and fake predictions with your own model.
+    """
+
     X_new = item.to_df()
     log.info(X_new)
     y_pred = random.choice([True, False])
