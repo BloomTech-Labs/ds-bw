433b9da4aecd6c1af1a97d27546d1490915dea73
DrewRust <drewrust1@gmail.com>
2020-08-23 05:14:52 -0400

    got original template working again

--- app/api/predict.py
+++ app/api/predict.py
@@ -1,12 +1,128 @@
+import sys
 import logging
 import random
-
 from fastapi import APIRouter
 import pandas as pd
 from pydantic import BaseModel, Field, validator
 
+# import json
+# import requests
+# import datetime
+# import base64
+# from urllib.parse import urlencode
+
+# client_id = 'b05f697aefd642bdb5fc256805542642'
+# client_secret = 'adba42b022da4e329779413a1194c558'
+
+# class SpotifyAPI(object):
+#     access_token = None
+#     access_token_expires = datetime.datetime.now()
+#     access_token_did_expire = True
+#     client_id = None
+#     client_secret = None
+#     token_url = "https://accounts.spotify.com/api/token"
+    
+#     def __init__(self, client_id, client_secret, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.client_id = client_id
+#         self.client_secret = client_secret
+
+#     def get_client_credentials(self):
+#         """
+#         Returns a base64 encoded string
+#         """
+#         client_id = self.client_id
+#         client_secret = self.client_secret
+#         if client_secret == None or client_id == None:
+#             raise Exception("You must set client_id and client_secret")
+#         client_creds = f"{client_id}:{client_secret}"
+#         client_creds_b64 = base64.b64encode(client_creds.encode())
+#         return client_creds_b64.decode()
+    
+#     def get_token_headers(self):
+#         client_creds_b64 = self.get_client_credentials()
+#         return {
+#             "Authorization": f"Basic {client_creds_b64}"
+#         }
+    
+#     def get_token_data(self):
+#         return {
+#             "grant_type": "client_credentials"
+#         } 
+    
+#     # added raise exception
+#     def perform_auth(self):
+#         token_url = self.token_url
+#         token_data = self.get_token_data()
+#         token_headers = self.get_token_headers()
+#         r = requests.post(token_url, data=token_data, headers=token_headers)
+#         if r.status_code not in range(200, 299):
+#             raise Exception("Could not authenticate client.")
+#             # return False
+#         data = r.json()
+#         now = datetime.datetime.now()
+#         access_token = data['access_token']
+#         expires_in = data['expires_in'] # seconds
+#         expires = now + datetime.timedelta(seconds=expires_in)
+#         self.access_token = access_token
+#         self.access_token_expires = expires
+#         self.access_token_did_expire = expires < now
+#         return True
+    
+#     def get_access_token(self):
+#         token = self.access_token
+#         expires = self.access_token_expires
+#         now = datetime.datetime.now()
+#         if expires < now:
+#             self.perform_auth()
+#             return self.get_access_token()
+#         elif token == None:
+#             self.perform_auth()
+#             return self.get_access_token() 
+#         return token
+    
+#     def get_resource_header(self):
+#         access_token = self.get_access_token()
+#         headers = {
+#             "Authorization": f"Bearer {access_token}"
+#         }
+#         return headers
+    
+    
+#     def get_features(self, song_id):
+#         endpoint = f"https://api.spotify.com/v1/audio-features/{song_id}"
+#         headers = self.get_resource_header()
+#         r = requests.get(endpoint, headers=headers)
+#         if r.status_code not in range(200, 299):
+#             return {}
+#         return r.json()
+  
+
+#     def search(self, query, search_type='track'): # type
+#         headers = self.get_resource_header()
+#         endpoint = "https://api.spotify.com/v1/search"
+#         data = urlencode({"q": query, "type": search_type.lower()})
+#         lookup_url = f"{endpoint}?{data}"
+#         r = requests.get(lookup_url, headers=headers)
+#         if r.status_code not in range(200, 299):  
+#             return {}
+#         return r.json()
+    
+    
+# spotify = SpotifyAPI(client_id, client_secret)
+# track1 = spotify.search("Master of Puppets", search_type="track")
+# songID = track1["tracks"]["items"][0]["id"]
+# name = track1["tracks"]["items"][0]["name"]
+# features = spotify.get_features(songID)
+
+
 log = logging.getLogger(__name__)
 router = APIRouter()
+
+# load_dotenv()
+
+# client_id = getenv('client_id')
+# client_secret = getenv('client_secret')
 
 
 class Item(BaseModel):
@@ -50,3 +166,4 @@ async def predict(item: Item):
         'prediction': y_pred,
         'probability': y_pred_proba
     }
+
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app.api import get_song, viz, predict
+from app.api import viz, predict
 
 app = FastAPI(
     title='DS API',
@@ -13,7 +13,7 @@ app = FastAPI(
 
 app.include_router(predict.router)
 app.include_router(viz.router)
-app.include_router(get_song.router)
+#app.include_router(get_song.router)
 
 app.add_middleware(
     CORSMiddleware,
