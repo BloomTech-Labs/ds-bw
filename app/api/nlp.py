c564d19b323425f5c01220e461e2db1edd39198d
Trevor James <trevorwillemjames@gmail.com>
2020-09-21 15:32:29 -0700

    scraping file updated

--- app/api/nlp.py
+++ app/api/nlp.py
@@ -8,5 +8,5 @@ function for calling sentiment score on data
 
 
 def sentiment_score(comment: str) -> float:
-    sentiment_dict = sia.polarity_scores(sentence)
+    sentiment_dict = sia.polarity_scores(comment)
     return sentiment_dict['compound']
--- app/api/scraping.py
+++ app/api/scraping.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from app.api.nlp import sentiment_score
 
 n_com = 100
 top_n = 10
@@ -7,18 +8,15 @@ page = requests.get("https://news.ycombinator.com/newcomments")
 soup = BeautifulSoup(page.content, features='html.parser')
 
 com_scores = []
-times = soup.find('a', class_='age')[:n_com]
-users = soup.find('a', class_='hnuser')[:n_com]
-coms = soup.find_all('div', class_='comment')[:n_com]
-
-for user in users:
-    print(user.get_text())
-
-for com in coms:
-    print(com.get_text())
-
-for time in times:
-    print(time.get_text())
+user_coms = zip(soup.find_all('a', class_='hnuser')[:n_com],
+                soup.find_all('div', class_='comment')[:n_com])
+for user, com in user_coms:
+    score = sentiment_score(com.get_text())
+    com_scores.append([user.get_text(), score])
+    print(com_scores)
+com_scores.sort(key=lambda x: x[1], reverse=True)
+for idx in range(len(com_scores)):
+    com_scores[idx].insert(0, idx+1)
 
 
 
